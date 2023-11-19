0e6d65556c32a1dc2a63fbf63e33eebdd43d353b
racheldunne28 <rmdunne@btconnect.com>
2023-11-19 15:34:06 +0000

    Make bike markers circles to easier to read

--- map.py
+++ map.py
@@ -7,17 +7,13 @@ from get_data import (get_bikepoints_json, get_bikepoints_locations, get_london_
 
 def gen_map(locations, ldn_geog, ldn_obesity):
     m = folium.Map(location=(51.51, 0.13))
-    for l in locations.keys():
-        folium.Marker(
-                location=[locations[l]['lat'], locations[l]['lon']],
-                tooltip=l,
-                icon=folium.Icon(icon='person-biking', prefix='fa'),
-            ).add_to(m)
     folium.Choropleth(geo_data=ldn_geog, name='choropleth', data=ldn_obesity, columns=['LAD22CD', 'Value'], key_on='feature.properties.LAD22CD',
                       fill_color="YlGn",
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name="Percentage living with obesity",).add_to(m)
+    legend_name="Percentage of adults living with obesity",).add_to(m)
+    for l in locations.keys():
+        folium.CircleMarker(location=[locations[l]['lat'], locations[l]['lon']], radius=4, tooltip=l, fill_color="blue", fill_opacity=0.8, color="black", weight=1).add_to(m)
     folium.LayerControl().add_to(m)
     m.save('outputs/map.html')
     return m
