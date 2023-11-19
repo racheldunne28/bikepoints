22ad78d38b6392d7b2e79be190218c0810be12a5
racheldunne28 <rmdunne@btconnect.com>
2023-11-19 15:25:41 +0000

    Add and map obesity data

--- get_data.py
+++ get_data.py
@@ -1,6 +1,8 @@
 import os
 
 import dotenv
+from fingertips_py import retrieve_data
+import geopandas
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -8,6 +10,7 @@ dotenv.load_dotenv()
 
 app_key = os.getenv('APP_KEY') # Primary key or secondary key from your TFL User profile, stored in a .env file in this folder
 
+latest_obesity_time_period = '2021/22'
 
 def get_bikepoints_json():
     '''
@@ -27,7 +30,34 @@ def get_bikepoints_locations(bikepoints_json):
     return {entry['commonName']: {'lat': entry['lat'], 'lon': entry['lon']} for entry in bikepoints_json}
 
 
+def get_london_las():
+    geog = geopandas.read_file('data/Local_Authority_Districts_December_2022_UK_BUC_V2_-5963189729337928393.geojson')
+    london_geog = geog[geog['LAD22CD'].str.contains('E09')]
+    return london_geog
+
+
+def get_london_adult_obesity():
+    obesity = retrieve_data.get_data_by_indicator_ids(['93088'], 301)
+    obesity = obesity[obesity['Age'] == '18+ yrs']
+    obesity = obesity[obesity['Sex'] == 'Persons']
+    obesity_london = obesity[obesity['Area Code'].str.contains('E09')]
+    obesity_latest = obesity_london[obesity_london['Time period'] == latest_obesity_time_period]
+    obesity_latest['Value'] = obesity_latest['Value'].astype(float)
+    return obesity_latest
+
+
+def get_joined_obesity_geog_data(ldn_obesity, ldn_geog):
+    ldn_obesity.rename({'Area Code': 'LAD22CD'}, axis=1, inplace=True)
+    ldn_obesity['LAD22CD'] = ldn_obesity['LAD22CD'].astype(str)
+    ldn_geog['LAD22CD'] = ldn_geog['LAD22CD'].astype(str)
+    joined = ldn_obesity.merge(ldn_geog, on='LAD22CD')
+    return joined
+
+
 if __name__ == "__main__":
     bikepoints_json = get_bikepoints_json()
     geog_data = get_bikepoints_locations(bikepoints_json)
-    print(geog_data)
+    ldn_obesity = get_london_adult_obesity()
+    ldn_geog = get_london_las()
+    joined = get_joined_obesity_geog_data(ldn_obesity, ldn_geog)
+    print(joined)
--- map.py
+++ map.py
@@ -1,12 +1,11 @@
 import folium
 
-from get_data import get_bikepoints_json, get_bikepoints_locations
+from get_data import (get_bikepoints_json, get_bikepoints_locations, get_london_las, 
+                      get_london_adult_obesity, get_joined_obesity_geog_data)
 
 
 
-if __name__ == '__main__':
-    bikepoints_json = get_bikepoints_json()
-    locations = get_bikepoints_locations(bikepoints_json)
+def gen_map(locations, ldn_geog, ldn_obesity):
     m = folium.Map(location=(51.51, 0.13))
     for l in locations.keys():
         folium.Marker(
@@ -14,4 +13,20 @@ if __name__ == '__main__':
                 tooltip=l,
                 icon=folium.Icon(icon='person-biking', prefix='fa'),
             ).add_to(m)
+    folium.Choropleth(geo_data=ldn_geog, name='choropleth', data=ldn_obesity, columns=['LAD22CD', 'Value'], key_on='feature.properties.LAD22CD',
+                      fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="Percentage living with obesity",).add_to(m)
+    folium.LayerControl().add_to(m)
     m.save('outputs/map.html')
+    return m
+
+
+if __name__ == '__main__':
+    bikepoints_json = get_bikepoints_json()
+    locations = get_bikepoints_locations(bikepoints_json)
+    ldn_geog = get_london_las()
+    ldn_obesity = get_london_adult_obesity()
+    obesity_geog = get_joined_obesity_geog_data(ldn_obesity, ldn_geog)
+    gen_map(locations, ldn_geog, ldn_obesity)
