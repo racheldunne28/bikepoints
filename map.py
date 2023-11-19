2b1a720023be8015aa9eb4c929788940ed9a8dfe
racheldunne28 <rmdunne@btconnect.com>
2023-11-19 19:59:02 +0000

    Change colour scheme so differences clearer

--- map.py
+++ map.py
@@ -8,7 +8,7 @@ from get_data import (get_bikepoints_json, get_bikepoints_locations, get_london_
 def gen_map(locations, ldn_geog, ldn_obesity, pop):
     m = folium.Map(location=(51.51, 0.13))
     cp = folium.Choropleth(geo_data=ldn_geog, name='choropleth', data=ldn_obesity, columns=['LAD22CD', 'Value'], key_on='feature.properties.LAD22CD',
-                      fill_color="YlGn", fill_opacity=0.7, line_opacity=0.2, legend_name="Percentage of adults living with obesity",).add_to(m)
+                      fill_color="YlGnBu", fill_opacity=0.7, line_opacity=0.2, legend_name="Percentage of adults living with obesity",).add_to(m)
     ldn_obesity_indexed = ldn_obesity.set_index('LAD22CD')
     for s in cp.geojson.data['features']:
         s['properties']['obesity'] = round(ldn_obesity_indexed.loc[s['properties']['LAD22CD'], 'Value'], 2)
