539018a669073a544a38054a673512c35cbd568a
racheldunne28 <rmdunne@btconnect.com>
2023-11-19 20:29:57 +0000

    tidying

--- get_data.py
+++ get_data.py
@@ -1,71 +1,91 @@
 import os
 
 import dotenv
-from fingertips_py import retrieve_data
 import geopandas
 import pandas as pd
 import requests
+from fingertips_py import retrieve_data
 from requests.auth import HTTPBasicAuth
 
 dotenv.load_dotenv()
 
-app_key = os.getenv('APP_KEY') # Primary key or secondary key from your TFL User profile, stored in a .env file in this folder
+app_key = os.getenv(
+    "APP_KEY"
+)  # Primary key or secondary key from your TFL User profile,
+# stored in a .env file in this folder
 
-latest_obesity_time_period = '2021/22'
+latest_obesity_time_period = "2021/22"
+
 
 def get_bikepoints_json():
-    '''
+    """
     Get basic bikepoints data from TFL bikepoints API
-    '''
-    auth = HTTPBasicAuth('app_key', app_key)
+    """
+    auth = HTTPBasicAuth("app_key", app_key)
     url = "https://api.tfl.gov.uk/BikePoint/"
-    hdr ={'Cache-Control': 'no-cache'}
+    hdr = {"Cache-Control": "no-cache"}
     response = requests.get(url, headers=hdr, auth=auth)
     return response.json()
 
 
 def get_bikepoints_locations(bikepoints_json):
-    '''
+    """
     Get latitude and longitude data from TFL bikepoints API JSON reponse
-    '''
-    return {entry['commonName']: {'lat': entry['lat'], 'lon': entry['lon']} for entry in bikepoints_json}
+    """
+    return {
+        entry["commonName"]: {"lat": entry["lat"], "lon": entry["lon"]}
+        for entry in bikepoints_json
+    }
 
 
 def get_london_las():
-    geog = geopandas.read_file('data/Local_Authority_Districts_December_2022_UK_BUC_V2_-5963189729337928393.geojson')
-    london_geog = geog[geog['LAD22CD'].str.contains('E09')]
+    geog = geopandas.read_file(
+        "data/Local_Authority_Districts_December_2022_" +
+        "UK_BUC_V2_-5963189729337928393.geojson"
+    )
+    london_geog = geog[geog["LAD22CD"].str.contains("E09")]
     return london_geog
 
 
 def get_london_adult_obesity():
-    obesity = retrieve_data.get_data_by_indicator_ids(['93088'], 301)
-    obesity = obesity[obesity['Age'] == '18+ yrs']
-    obesity = obesity[obesity['Sex'] == 'Persons']
-    obesity_london = obesity[obesity['Area Code'].str.contains('E09')]
-    obesity_latest = obesity_london[obesity_london['Time period'] == latest_obesity_time_period]
-    obesity_latest['Value'] = obesity_latest['Value'].astype(float)
+    obesity = retrieve_data.get_data_by_indicator_ids(["93088"], 301)
+    obesity = obesity[obesity["Age"] == "18+ yrs"]
+    obesity = obesity[obesity["Sex"] == "Persons"]
+    obesity_london = obesity[obesity["Area Code"].str.contains("E09")]
+    obesity_latest = obesity_london[
+        obesity_london["Time period"] == latest_obesity_time_period
+    ]
+    obesity_latest.loc[:, "Value"] = obesity_latest.loc[:, "Value"].astype(float)
+    obesity_latest.rename({'Area Code': 'LAD22CD'}, axis=1, inplace=True)
     return obesity_latest
 
 
 def get_pop():
-    pop = pd.read_csv('data/TS007-2021-3.csv')
-    total_pop = pop[['Lower tier local authorities Code', 'Observation']].groupby('Lower tier local authorities Code').sum()
+    pop = pd.read_csv("data/TS007-2021-3.csv")
+    total_pop = (
+        pop[["Lower tier local authorities Code", "Observation"]]
+        .groupby("Lower tier local authorities Code")
+        .sum()
+    )
     return total_pop
 
 
 def get_deprivation():
-    imd = pd.read_excel('data/File_10_-_IoD2019_Local_Authority_District_Summaries__lower-tier__.xlsx', sheet_name='Health')
-    imd.rename({'Health Deprivation and Disability - Proportion of LSOAs in most deprived 10% nationally ': 'health_deprivation'}, axis=1, inplace=True)
-    imd.set_index('Local Authority District code (2019)', inplace=True)
-    return imd[['health_deprivation']]
-
-
-def get_joined_obesity_geog_data(ldn_obesity, ldn_geog):
-    ldn_obesity.rename({'Area Code': 'LAD22CD'}, axis=1, inplace=True)
-    ldn_obesity['LAD22CD'] = ldn_obesity['LAD22CD'].astype(str)
-    ldn_geog['LAD22CD'] = ldn_geog['LAD22CD'].astype(str)
-    joined = ldn_obesity.merge(ldn_geog, on='LAD22CD')
-    return joined
+    imd = pd.read_excel(
+        "data/File_10_-_IoD2019_Local_Authority_" +
+        "District_Summaries__lower-tier__.xlsx",
+        sheet_name="Health",
+    )
+    imd.rename(
+        {
+            "Health Deprivation and Disability - Proportion " +
+            "of LSOAs in most deprived 10% nationally ": "health_deprivation"
+        },
+        axis=1,
+        inplace=True,
+    )
+    imd.set_index("Local Authority District code (2019)", inplace=True)
+    return imd[["health_deprivation"]]
 
 
 if __name__ == "__main__":
@@ -73,6 +93,5 @@ if __name__ == "__main__":
     geog_data = get_bikepoints_locations(bikepoints_json)
     ldn_obesity = get_london_adult_obesity()
     ldn_geog = get_london_las()
-    joined = get_joined_obesity_geog_data(ldn_obesity, ldn_geog)
     pop = get_pop()
     imd = get_deprivation()
--- map.py
+++ map.py
@@ -1,34 +1,63 @@
 import folium
 
-from get_data import (get_bikepoints_json, get_bikepoints_locations, get_london_las, 
-                      get_london_adult_obesity, get_joined_obesity_geog_data, get_pop,
-                      get_deprivation)
-
+from get_data import (get_bikepoints_json, get_bikepoints_locations,
+                      get_deprivation, get_london_adult_obesity,
+                      get_london_las, get_pop)
 
 
 def gen_map(locations, ldn_geog, ldn_obesity, pop, imd):
     m = folium.Map(location=(51.51, 0.13))
-    cp = folium.Choropleth(geo_data=ldn_geog, name='choropleth', data=ldn_obesity, columns=['LAD22CD', 'Value'], key_on='feature.properties.LAD22CD',
-                      fill_color="YlGnBu", fill_opacity=0.7, line_opacity=0.2, legend_name="Percentage of adults living with obesity",).add_to(m)
-    ldn_obesity_indexed = ldn_obesity.set_index('LAD22CD')
-    for s in cp.geojson.data['features']:
-        s['properties']['obesity'] = round(ldn_obesity_indexed.loc[s['properties']['LAD22CD'], 'Value'], 2)
-        s['properties']['pop'] = f"{int(pop.loc[s['properties']['LAD22CD'], 'Observation']):,}"   
-        s['properties']['health_deprivation'] = f"{imd.loc[s['properties']['LAD22CD'], 'health_deprivation']:.2%}"
-    folium.GeoJsonTooltip(['LAD22NM', 'obesity', 'pop', 'health_deprivation'], aliases=['Local Authority', r'% adults overweight or obese', 'Population', '% LSOAs in 10% most health deprived']).add_to(cp.geojson)
-    for l in locations.keys():
-        folium.CircleMarker(location=[locations[l]['lat'], locations[l]['lon']], radius=4, tooltip=l, fill_color="blue", fill_opacity=0.8, color="black", weight=1).add_to(m)
+    cp = folium.Choropleth(
+        geo_data=ldn_geog,
+        name="choropleth",
+        data=ldn_obesity,
+        columns=["LAD22CD", "Value"],
+        key_on="feature.properties.LAD22CD",
+        fill_color="YlGnBu",
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name="Percentage of adults living with obesity",
+    ).add_to(m)
+    ldn_obesity_indexed = ldn_obesity.set_index("LAD22CD")
+    for s in cp.geojson.data["features"]:
+        s["properties"]["obesity"] = round(
+            ldn_obesity_indexed.loc[s["properties"]["LAD22CD"], "Value"], 2
+        )
+        s["properties"][
+            "pop"
+        ] = f"{int(pop.loc[s['properties']['LAD22CD'], 'Observation']):,}"
+        s["properties"][
+            "health_deprivation"
+        ] = f"{imd.loc[s['properties']['LAD22CD'], 'health_deprivation']:.2%}"
+    folium.GeoJsonTooltip(
+        ["LAD22NM", "obesity", "pop", "health_deprivation"],
+        aliases=[
+            "Local Authority",
+            r"% adults overweight or obese",
+            "Population",
+            "% LSOAs in 10% most health deprived",
+        ],
+    ).add_to(cp.geojson)
+    for location in locations.keys():
+        folium.CircleMarker(
+            location=[locations[location]["lat"], locations[location]["lon"]],
+            radius=4,
+            tooltip=location,
+            fill_color="blue",
+            fill_opacity=0.8,
+            color="black",
+            weight=1,
+        ).add_to(m)
     folium.LayerControl().add_to(m)
-    m.save('outputs/map.html')
+    m.save("outputs/map.html")
     return m
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bikepoints_json = get_bikepoints_json()
     locations = get_bikepoints_locations(bikepoints_json)
     ldn_geog = get_london_las()
     ldn_obesity = get_london_adult_obesity()
-    obesity_geog = get_joined_obesity_geog_data(ldn_obesity, ldn_geog)
     pop = get_pop()
     imd = get_deprivation()
     gen_map(locations, ldn_geog, ldn_obesity, pop, imd)
